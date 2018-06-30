c074269080ebf266d34dd26b1ca630599f4b7b36
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-06-30 13:39:52 -0700

    add batches service

---
+++ data-partitioner/data-partitioner.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+tf.enable_eager_execution()
+
+base = '/data/tf-records/'
+
+melanoma = tf.data.TFRecordDataset(base + 'melanoma.tfrecords')
+
+not_melanoma = tf.data.TFRecordDataset(base + 'not_melanoma.tfrecords')
+
+for not_melanoma_length, _ in enumerate(not_melanoma):
+    continue
+
+melanoma_repeated = melanoma.repeat().take(not_melanoma_length)
+
+dataset = melanoma_repeated.concatenate(not_melanoma) \
+                           .shuffle(25000).batch(1000)
+
+for i, examples in enumerate(dataset):
+    filename = base + 'batches/batch_{}.tfrecords'.format(i)
+    with tf.python_io.TFRecordWriter(filename) as writer:
+        for example in examples:
+            writer.write(example.numpy())
