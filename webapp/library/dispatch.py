8195d7b0d4549a922dc00435fb50771ea90b3661
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-08-09 11:24:38 -0700

    update license

--- webapp/library/dispatch.py
+++ webapp/library/dispatch.py
@@ -1,13 +1,12 @@
-# Copyright (c) 2018 Adam Kowalski
-# This code is available under the "Apache License 2.0"
-# Please see the file COPYING in this distribution for license terms.
+# https://adambard.com/blog/implementing-multimethods-in-python/
+# These two methods are available for public use on the blog mentioned above
+
 
 def multi(dispatch_fn):
     def _inner(*args, **kwargs):
-        return _inner.__multi__.get(
-            dispatch_fn(*args, **kwargs),
-            _inner.__multi_default__
-        )(*args, **kwargs)
+        result = dispatch_fn(*args, **kwargs)
+        default = _inner.__multi_default__
+        return _inner.__multi__.get(result, default)(*args, **kwargs)
 
     _inner.__multi__ = {}
     _inner.__multi_default__ = lambda *args, **kwargs: None
@@ -21,4 +20,5 @@ def method(dispatch_fn, dispatch_key=None):
         else:
             dispatch_fn.__multi__[dispatch_key] = fn
         return dispatch_fn
+
     return apply_decorator
