395f6c40ac52178d8337fe15316c06dbdec4ef35
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-08-10 10:31:48 -0700

    add launch and shutdown scripts

---
+++ shutdown.sh
@@ -0,0 +1,5 @@
+# Copyright (c) 2018 Adam Kowalski
+# This code is available under the "Apache License 2.0"
+# Please see the file COPYING in this distribution for license terms.
+
+docker-compose -f webapp/docker-compose.yml down
